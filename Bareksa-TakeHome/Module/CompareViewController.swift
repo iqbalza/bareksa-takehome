06aa7a11ede0aa577e7b161a20643c557b37d02f
Iqbal Zauqul <iqbalzauqul@gmail.com>
2022-01-19 14:26:28 +0700

    Add compare module

--- Bareksa-TakeHome/Module/CompareViewController.swift
+++ Bareksa-TakeHome/Module/CompareViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CompareViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Bareksa-TakeHome/Module/CompareViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CompareViewModel.swift
+//  Bareksa-TakeHome
+//
+//  Created by Iqbal Zauqul Adib on 19/01/22.
+//
+
+import Foundation
