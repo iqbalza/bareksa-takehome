eaa76bce1285dc2226390d233fd4972726a2d021
Iqbal Zauqul <iqbalzauqul@gmail.com>
2022-01-22 04:53:31 +0700

    Add markerview

--- Bareksa-TakeHome/Common/CustomMarker.swift
+++ Bareksa-TakeHome/Common/CustomMarker.swift
@@ -7,24 +7,54 @@
 
 import Foundation
 import Charts
+import SnapKit
+import UIKit
 
-class CircleMarker: MarkerImage {
+class CircleMarker: MarkerView {
     
-    @objc var color: UIColor
-    @objc var radius: CGFloat = 4
+    lazy var view: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: 15))
+        view.layer.cornerRadius = view.frame.width/2
+        view.clipsToBounds = true
+        return view
+    }()
     
-    @objc public init(color: UIColor) {
-        self.color = color
-        super.init()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
     }
     
-    override func draw(context: CGContext, point: CGPoint) {
-        let circleRect = CGRect(x: point.x - radius, y: point.y - radius, width: radius * 2, height: radius * 2)
-        context.setFillColor(color.cgColor)
-        context.fillEllipse(in: circleRect)
-        
-        context.restoreGState()
+    required init?(coder: NSCoder) {
+            super.init(coder: coder)
+            setup()
+        }
+    
+    override func refreshContent(entry: ChartDataEntry, highlight: Highlight) {
+        print("Content Refresh")
+        let index = entry.data as! Int
+        switch index {
+        case 0:
+            view.backgroundColor = .lineGreen
+        case 1:
+            view.backgroundColor = .linePurple
+         default:
+            view.backgroundColor = .lineBlue
+
+
+
+        }
     }
+    
+    func setup() {
+
+        addSubview(view)
+        self.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        self.offset = CGPoint(x: -(self.frame.width/2), y: -self.frame.height/2)
+    }
+    
+
     
     
 }
+
+
