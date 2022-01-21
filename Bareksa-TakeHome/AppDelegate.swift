b43d9d80133a58f9c899389b1417a8679238175b
Iqbal Zauqul <iqbalzauqul@gmail.com>
2022-01-21 11:53:52 +0700

    Set root vc

--- Bareksa-TakeHome/AppDelegate.swift
+++ Bareksa-TakeHome/AppDelegate.swift
@@ -13,6 +13,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window : UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = CompareViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
              return true
     }
 }
